e94e37278d2fe0b2a396173c35d6be332071cc3c
lucas-rib-oli <100383154@alumnos.uc3m.es>
2023-01-28 12:00:09 +0100

    prediction: add several changes to get the heading and velocity to do the prediction
    Now we usa heading and velocities to train the model

--- prediction_forecasting.py
+++ prediction_forecasting.py
@@ -3,9 +3,10 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from pathlib import Path
-from av2.datasets.motion_forecasting import scenario_serialization
+from av2.datasets.motion_forecasting import scenario_serialization, data_schema
 from av2.datasets.motion_forecasting.data_schema import ArgoverseScenario, ObjectType
-from model.transformer import TransformerModel
+from av2_motion_forecasting_dataset import Av2MotionForecastingDataset
+from model.transformer import TransformerModel, generate_square_subsequent_mask
 from typing import Final, Dict, List
 from collections import defaultdict
 from colorama import Fore
@@ -35,7 +36,7 @@ parser.add_argument('--path_2_configuration', type=str, default='config_transfor
 args = parser.parse_args()
 
 class TransformerPrediction ():
-    def __init__(self, config : dict, num_scenarios: int = 40) -> None:
+    def __init__(self, config : dict, num_scenarios: int = 100) -> None:
 
         self.d_model = config['d_model']
         self.nhead = config['nhead']
@@ -49,7 +50,7 @@ class TransformerPrediction ():
         self.num_epochs = config['num_epochs']
         self.learning_rate = config['lr']
         self.current_lr = self.learning_rate
-        self.batch_size = 1
+        self.batch_size = 2048
         self.sequence_length = config['sequence_length']
         self.num_workers = config['num_workers']
         self.dropout = config['dropout']
@@ -61,21 +62,31 @@ class TransformerPrediction ():
         
         self.model = TransformerModel (enc_inp_size=self.enc_inp_size, dec_inp_size=self.dec_inp_size, dec_out_size=self.dec_out_size, 
                                        d_model=self.d_model, nhead=self.nhead, N=self.num_encoder_layers, dim_feedforward=self.dim_feedforward).to(self.device)
-
+        # ---------------------------------------------------------------------------------------------------- #
+        # self.val_data = Av2MotionForecastingDataset (dataset_dir=args.path_2_dataset, split='val', sequence_length=self.sequence_length, 
+        #                                              load_pickle=False, save_pickle=False)
+        # self.val_dataloader = DataLoader (self.val_data, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True)
+        # ---------------------------------------------------------------------------------------------------- #
+        self.loss_fn = nn.HuberLoss(reduction='mean')
+        # ---------------------------------------------------------------------------------------------------- #
         argoverse_scenario_dir = os.path.join(args.path_2_dataset, 'argoverse2', 'motion_forecasting', 'train')
         argoverse_scenario_dir = Path(argoverse_scenario_dir)
         all_scenario_files = sorted(argoverse_scenario_dir.rglob("*.parquet"))
-        all_scenario_files = all_scenario_files[:num_scenarios]
+        all_scenario_files = all_scenario_files[200:300]
         
         self.src_actor_trajectory_by_id: Dict[str, npt.NDArray] = {}
         self.tgt_actor_trajectory_by_id: Dict[str, npt.NDArray] = {}
+        
+        self.src_actor_trajectory_by_scenarios : Dict[str, npt.NDArray] = {}
+        self.tgt_actor_trajectory_by_scenarios : Dict[str, npt.NDArray] = {}
+        
         # ---------------------------------------------------------------------------------------------------- #
         idx = 0
         begin = 0
         end = 0
         threads = list()
         scenario_path_splitted = list()
-        num_threads = 32
+        num_threads = 1
         length = int (len(all_scenario_files) / num_threads)
         remainder = int (len(all_scenario_files) % num_threads)
         
@@ -98,6 +109,9 @@ class TransformerPrediction ():
         # Join threads
         for index, thread in track(enumerate(threads)):
             thread.join()
+        
+        # for scenario_path in track(all_scenario_files):
+        #     self.__generate_scenario(scenario_path)
         
         print (Fore.CYAN + 'Device: ' + Fore.WHITE + self.device + Fore.RESET)
         print (Fore.CYAN + 'Experiment name: ' + Fore.WHITE + self.experiment_name + Fore.RESET)
@@ -127,8 +141,9 @@ class TransformerPrediction ():
         scenario_tgt_actor_trajectory_by_id: Dict[str, npt.NDArray] = {}
         
         # Get trajectories
+        idx = 0
         for track in scenario.tracks:
-            # Only get the vehicles and dont save the ego-vehicle (AV)
+            # Only get the vehicles and dont save the ego-vehicle (AV), and 
             if track.object_type != ObjectType.VEHICLE or track.track_id == "AV":
                 continue
             # Only get the 'FOCAL TACK' and 'SCORED CARS'
@@ -147,20 +162,23 @@ class TransformerPrediction ():
             # Get target actor trajectory and heading history -> forescated or predicted trajectory
             tgt_actor_trajectory = actor_state[_OBS_DURATION_TIMESTEPS:_TOTAL_DURATION_TIMESTEPS]
             
+            idx += 1
             scenario_src_actor_trajectory_by_id[track.track_id] = src_actor_trajectory
             scenario_tgt_actor_trajectory_by_id[track.track_id] = tgt_actor_trajectory
         
         if scenario.focal_track_id in scenario_tgt_actor_trajectory_by_id.keys():
             # Get the final observed trajectory of the focal agent
             focal_coordinate = scenario_src_actor_trajectory_by_id[scenario.focal_track_id][-1, 0:2]
-            
+                        
             src_full_traj = scenario_src_actor_trajectory_by_id[scenario.focal_track_id][:, 0:2]
             tgt_full_traj = scenario_tgt_actor_trajectory_by_id[scenario.focal_track_id][:, 0:2]
             
             heading_vector = scenario_src_actor_trajectory_by_id[scenario.focal_track_id][-1, 2:4]
             sin_heading = heading_vector[0]
             cos_heading = heading_vector[1]
-            
+            # Get heading
+            focal_heading = np.arctan2(sin_heading,
+                                       cos_heading)
             
             src_zeros_vector = np.zeros((src_full_traj.shape[0], 1))
             src_ones_vector = np.ones((src_full_traj.shape[0], 1))
@@ -171,7 +189,7 @@ class TransformerPrediction ():
                                    [sin_heading,  cos_heading, 0, 0],
                                    [          0,            0, 1, 0],
                                    [          0,            0, 0, 1]])
-
+              
             scenario_src_actor_trajectory_by_id_transformed = {}
             scenario_tgt_actor_trajectory_by_id_transformed = {}
             # Transform all trajectories
@@ -179,11 +197,22 @@ class TransformerPrediction ():
                 src_agent_coordinate = scenario_src_actor_trajectory_by_id[track_id][:, 0:2]
                 tgt_agent_coordinate = scenario_tgt_actor_trajectory_by_id[track_id][:, 0:2]
                 
+                src_agent_heading = np.arctan2(scenario_src_actor_trajectory_by_id[track_id][:,2], scenario_src_actor_trajectory_by_id[track_id][:,3])
+                tgt_agent_heading = np.arctan2(scenario_tgt_actor_trajectory_by_id[track_id][:,2], scenario_tgt_actor_trajectory_by_id[track_id][:,3])
+                
+                src_agent_velocity = scenario_src_actor_trajectory_by_id[track_id][:, 4:]
+                tgt_agent_velocity = scenario_tgt_actor_trajectory_by_id[track_id][:, 4:]
+                
                 # Add Z --> 0
                 src_agent_coordinate = np.append (src_agent_coordinate, src_zeros_vector, axis=1)
                 src_agent_coordinate = np.append (src_agent_coordinate, src_ones_vector, axis=1)
                 tgt_agent_coordinate = np.append (tgt_agent_coordinate, tgt_zeros_vector, axis=1)
                 tgt_agent_coordinate = np.append (tgt_agent_coordinate, tgt_ones_vector, axis=1)
+                
+                src_agent_velocity = np.append (src_agent_velocity, src_zeros_vector, axis=1)
+                src_agent_velocity = np.append (src_agent_velocity, src_ones_vector, axis=1)
+                tgt_agent_velocity = np.append (tgt_agent_velocity, tgt_zeros_vector, axis=1)
+                tgt_agent_velocity = np.append (tgt_agent_velocity, tgt_ones_vector, axis=1)
                 # Substract the center
                 src_agent_coordinate = src_agent_coordinate - np.append (focal_coordinate, [0, 0])
                 tgt_agent_coordinate = tgt_agent_coordinate - np.append (focal_coordinate, [0, 0])
@@ -191,14 +220,28 @@ class TransformerPrediction ():
                 # Transformed trajectory
                 src_agent_coordinate_tf = np.dot(rot_matrix, src_agent_coordinate.T).T
                 tgt_agent_coordinate_tf = np.dot(rot_matrix, tgt_agent_coordinate.T).T
+                # Transformed velocitys
+                src_agent_velocity_tf = np.dot(rot_matrix, src_agent_velocity.T).T
+                tgt_agent_velocity_tf = np.dot(rot_matrix, tgt_agent_velocity.T).T
+                src_agent_velocity_tf = src_agent_velocity_tf[:,0:2] # Get only the components
+                tgt_agent_velocity_tf = tgt_agent_velocity_tf[:,0:2]
+                # Transformed heading
+                src_agent_heading_tf = src_agent_heading - focal_heading
+                tgt_agent_heading_tf = tgt_agent_heading - focal_heading
+                # Normalice heading [-pi, pi)
+                src_agent_heading_tf = (src_agent_heading_tf + np.pi) % (2 * np.pi) - np.pi
+                tgt_agent_heading_tf = (tgt_agent_heading_tf + np.pi) % (2 * np.pi) - np.pi
+                # Vector heading
+                src_agent_vector_heading_tf = np.array([np.sin(src_agent_heading_tf), np.cos(src_agent_heading_tf)])
+                tgt_agent_vector_heading_tf = np.array([np.sin(tgt_agent_heading_tf), np.cos(tgt_agent_heading_tf)])
                 
                 # Add heading
-                src_agent_coordinate_tf[:,2:4] = scenario_src_actor_trajectory_by_id[track_id][:, 2:4]
-                tgt_agent_coordinate_tf[:,2:4] = scenario_tgt_actor_trajectory_by_id[track_id][:, 2:4]
+                src_agent_coordinate_tf[:,2:4] = src_agent_vector_heading_tf.T
+                tgt_agent_coordinate_tf[:,2:4] = tgt_agent_vector_heading_tf.T
                 
                 # Add velocity
-                src_agent_coordinate_tf = np.append (src_agent_coordinate_tf, scenario_src_actor_trajectory_by_id[track_id][:, 4:], axis=1)
-                tgt_agent_coordinate_tf = np.append (tgt_agent_coordinate_tf, scenario_tgt_actor_trajectory_by_id[track_id][:, 4:], axis=1)
+                src_agent_coordinate_tf = np.append (src_agent_coordinate_tf, src_agent_velocity_tf, axis=1)
+                tgt_agent_coordinate_tf = np.append (tgt_agent_coordinate_tf, tgt_agent_velocity_tf, axis=1)
                 
                 # Save the trajectory
                 self.src_actor_trajectory_by_id[track_id] = src_agent_coordinate_tf
@@ -262,6 +305,52 @@ class TransformerPrediction ():
                     # plt.legend(loc="upper left")
             plt.show()
                 # exit(0)
+    # ---------------------------------------------------------------------------------------------------- #
+    def compute_loss_val (self):
+        # Set the network in evaluation mode
+        self.model.eval()
+        
+        validation_losses = []
+        for idx, data in track(enumerate(self.val_dataloader)):
+            # Set no requires grad
+            with torch.no_grad():
+                src: torch.Tensor = data['src']
+                tgt: torch.Tensor = data['tgt']
+                src = src.to(self.device)
+                # src = src.double()
+                tgt = tgt.to(self.device)
+                # tgt = tgt.double()
+                
+                # Generate a square mask for the sequence
+                src_mask = torch.zeros(src.size()[1], src.size()[1]).to(self.device)
+                
+                # Get the start of the sequence
+                # dec_inp = torch.zeros()
+                # dec_inp = torch.zeros(tgt.shape[0], 1, tgt.shape[2]).to(self.device).double()
+                dec_inp = tgt[:, 0, :]
+                # Implement one dimension for the tranformer to be able to deal with the input decoder
+                dec_inp = dec_inp.unsqueeze(1).to(self.device)
+                # ----------------------------------------------------------------------- #
+                # Encode step
+                memory = self.model.encode(src, src_mask).to(self.device)
+                # ----------------------------------------------------------------------- #
+            
+                # Apply Greedy Code
+                for _ in range (0, self.sequence_length - 1):
+                    # Get target mask
+                    tgt_mask = (nn.Transformer.generate_square_subsequent_mask(dec_inp.size()[1])).to(self.device)
+                    # Get tokens
+                    out = self.model.decode(dec_inp, memory, tgt_mask).to(self.device)
+                    # Generate the prediction
+                    prediction = self.model.generate ( out ).to(self.device)
+                    # Concatenate
+                    dec_inp = torch.cat([dec_inp, prediction[:, -1:, :]], dim=1).to(self.device)
+                loss = self.loss_fn (dec_inp, tgt)
+                loss = loss.mean()
+                validation_losses.append(loss.detach().cpu().numpy())
+        validation_loss = np.mean(validation_losses)
+        print ('loss val: ', validation_loss)
+        
                 
             
 # ===================================================================================== #
