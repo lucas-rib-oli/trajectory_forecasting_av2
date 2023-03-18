b6455ad8a9b780917cf2ff4ba38f49e52c2f8206
lucas-rib-oli <100383154@alumnos.uc3m.es>
2023-03-18 11:13:41 +0100

    config: config to predict one trajectory and 60 frames

--- configs/config_files/transtraj_config.py
+++ configs/config_files/transtraj_config.py
@@ -2,14 +2,14 @@
 train = dict(
     device = "cuda:0",
     num_workers = 8,
-    experiment_name = "TrajClassification_sumLoss",
-    num_epochs = 1000,
-    batch_size = 512,
+    experiment_name = "TrajClassification_euclideanLoss",
+    num_epochs = 200,
+    batch_size = 1024,
     resume_train = False,
 )
 
 optimizer = dict(
-    lr = 1e-5,
+    lr = 2e-4,
     opt_warmup = 5,
     opt_factor = 0.1
 )
@@ -24,10 +24,10 @@ model = dict(
     type='TransTraj',
     pose_dim = 6, # features dim [x, y, ...]
     history_size = 8,
-    future_size = 12, # Output trajectory size || 12 output poses
+    future_size = 60, # Output trajectory size || 12 output poses
     num_queries = 1, # Number of trajectories of a target || K = 6
-    dec_out_size = 6*12, # 6 * 12
-    d_model = 128,
+    dec_out_size = 2*60, # 6 * 12
+    d_model = 64,
     nhead = 2,
     N = 2, # Numer of decoder/encoder layers
     dim_feedforward = 256,
