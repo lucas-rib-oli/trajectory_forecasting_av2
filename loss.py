5935768d7a0321803f0d9b44a06d96b73bc29fa6
lucas-rib-oli <100383154@alumnos.uc3m.es>
2023-03-18 11:01:44 +0100

    loss: include the loss with respect to the closest trajectory

--- loss.py
+++ loss.py
@@ -16,8 +16,25 @@ class TransLoss (nn.Module):
         one_hot_vector = torch.zeros_like (distance_summ).to(pred_trajs.device)
         row_indices = np.arange(distance_summ.shape[0])
         one_hot_vector[row_indices, index] += torch.ones(len(row_indices)).to(pred_trajs.device)
-        return one_hot_vector
-        
+        return one_hot_vector, index    
+    # ===================================================================================== #
+    def closest_trajectory_loss (self, pred: torch.Tensor, gt: torch.Tensor):
+        """Compute the regresion loss with with respect to the closest predicted trajectory
+
+        Args:
+            pred (torch.Tensor): _description_
+            gt (torch.Tensor): _description_
+        """
+        bs = pred.shape[0]
+        # Compute distance
+        dist = torch.mean(torch.abs(gt - pred), dim=(2, 3))
+        # finde the closest trajectory to the gt 
+        min_dist, min_idx = torch.min(dist, dim=1)
+        # Get the closest trajectory
+        closest_traj = torch.gather(pred, 1, min_idx.view(bs, 1, 1, 1).repeat(1, 1, 60, 2))
+        # Compute loss
+        reg_loss = self.reg_loss_fn(closest_traj, gt)
+        return reg_loss
     # ===================================================================================== #
     def forward (self, pred_trajs: torch.Tensor, pred_scores: torch.Tensor, gt_trajs: torch.Tensor) -> torch.Tensor:
         """Forward function
