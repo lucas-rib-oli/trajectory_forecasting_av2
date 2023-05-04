ce0dbc4c846343438f873e4bdec4c7c339a78dd2
lucas-rib-oli <100383154@alumnos.uc3m.es>
2023-05-04 22:24:54 +0200

    losses: change the loss to get the closest trajectory taking into account the multiple agents

--- losses/closestL2.py
+++ losses/closestL2.py
@@ -11,28 +11,33 @@ class ClosestL2Loss (nn.Module):
     # ===================================================================================== #   
     def get_one_hot_vector_by_distance (self, pred_trajs: torch.Tensor, gt_trajs: torch.Tensor) -> torch.Tensor:
         # Get the distance between the prediction and the gt trajectories
-        squared_dis = torch.sum(torch.pow(pred_trajs[:,:,:,0:2] - gt_trajs[:,:,:,0:2], 2), dim=-1)
-        distance_summ = torch.sum(squared_dis, dim=-1)
-        val, index = torch.min(distance_summ,dim=-1)
-        one_hot_vector = torch.zeros_like (distance_summ).to(pred_trajs.device)
-        row_indices = np.arange(distance_summ.shape[0])
-        one_hot_vector[row_indices, index] += torch.ones(len(row_indices)).to(pred_trajs.device)
-        return one_hot_vector, index    
+        dist = torch.norm(gt_trajs[:,:,:,:2] - pred_trajs[:,:,:,:2], p=2, dim=-1)
+        distance_sum = torch.sum(dist, dim=-1)
+        indexes = torch.argmin(distance_sum,dim=-1)
+        one_hot_vector = torch.zeros_like (distance_sum).to(pred_trajs.device)
+        one_hot_vector = one_hot_vector.scatter(2, indexes.unsqueeze(-1), 1)
+        return one_hot_vector, indexes    
     # ===================================================================================== #
-    def closest_trajectory_loss (self, pred: torch.Tensor, gt: torch.Tensor):
-        """Compute the regresion loss with with respect to the closest predicted trajectory
+    def closest_trajectory_loss (self, pred: torch.Tensor, gt_overdim: torch.Tensor):
+        """Compute the regresion loss with respect to the closest predicted trajectory
 
         Args:
             pred (torch.Tensor): _description_
             gt (torch.Tensor): _description_
         """
         bs = pred.shape[0]
+        timesteps = pred.shape[-2]
+        feat_dim = pred.shape[-1]
         # Compute distance
-        dist = torch.mean(torch.abs(gt - pred), dim=(2, 3))
+        dist = torch.norm(gt_overdim[:,:,:,:2] - pred[:,:,:,:2], p=2, dim=-1)
+        
         # finde the closest trajectory to the gt 
-        min_dist, min_idx = torch.min(dist, dim=1)
+        min_idx = torch.argmin(torch.sum(dist, dim=-1), dim=-1)
+        indexes = min_idx.view(bs, -1, 1, 1, 1).repeat(1,1,1,timesteps, feat_dim)
         # Get the closest trajectory
-        closest_traj = torch.gather(pred, 1, min_idx.view(bs, 1, 1, 1).repeat(1, 1, 60, 2))
+        closest_traj = torch.gather(pred, 2, indexes).squeeze(2) # Squeeze the trajectory index
+        # Get the gt with only one trajectory
+        gt = gt_overdim[:,:,0]
         
         # import matplotlib.pyplot as plt
         # trajs_pred_0 = pred[0]
@@ -57,25 +62,29 @@ class ClosestL2Loss (nn.Module):
             offset_gt_trajs (torch.Tensor): _description_
         """
         bs = pred_trajs.shape[0]
-        future_traj_size = pred_trajs.shape[2]
+        K = pred_trajs.shape[2] # Number trajectory of a target
+        
+        future_traj_size = pred_trajs.shape[3]
         output_pose_dim = pred_trajs.shape[-1]
-        K = pred_trajs.shape[1] # Number trajectory of a target
         # ----------------------------------------------------------------------- #
-        gt_overdim: torch.Tensor = gt_trajs.unsqueeze(1).repeat(1, K, 1, 1)
+        gt_overdim: torch.Tensor = gt_trajs.unsqueeze(2).repeat(1, 1, K, 1, 1)
         # offset_gt_trajs_overdim: torch.Tensor = offset_gt_trajs.unsqueeze(1).repeat(1, K, 1, 1)
         # ----------------------------------------------------------------------- #
         # Compute classification loss
-        one_hot_vector, index = self.get_one_hot_vector_by_distance (pred_trajs, gt_overdim)
+        one_hot_vector, _ = self.get_one_hot_vector_by_distance (pred_trajs, gt_overdim)
         cls_los = self.class_loss_fn(pred_scores, one_hot_vector)
         # ----------------------------------------------------------------------- #        
         # Compute regresion loss
         # Get the loss with respect the best prediction
-        reg_loss = self.closest_trajectory_loss(pred_trajs[:,:,:,:2], gt_overdim[:,:,:,:2])
+        reg_loss = self.closest_trajectory_loss(pred_trajs, gt_overdim)
         
         # reg_loss = self.reg_loss_fn (pred_trajs, gt_overdim[:,:,:,:2])
         # reduce the loss if is neccesary
         if len(reg_loss.shape) > 2:
            reg_loss = torch.sum(reg_loss, -1)
+           # Sum agents loss
+           reg_loss = torch.sum(reg_loss, -1)
+           # Compute the mean with the batch size
            reg_loss = torch.mean(reg_loss)
         elif len(reg_loss.shape) > 1:
             reg_loss = torch.mean(reg_loss)        
