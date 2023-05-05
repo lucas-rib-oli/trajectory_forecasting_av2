81f089b9e1d7c5e1a7dd8e0294b8415df14095dd
lucas-rib-oli <100383154@alumnos.uc3m.es>
2023-05-05 22:20:12 +0200

    model: pos encoding: fix positional encoding to get the input with [bs, feat]

--- model/transtraj.py
+++ model/transtraj.py
@@ -163,17 +163,17 @@ class PositionalEncoding(nn.Module):
 
         position = torch.arange(max_len).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
-        pe = torch.zeros(max_len, 1, d_model)
-        pe[:, 0, 0::2] = torch.sin(position * div_term)
-        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        pe = torch.zeros(max_len, d_model)
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
         self.register_buffer('pe', pe)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         Args:
-            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+            x: Tensor, shape [batch_size, seq_len, embedding_dim]
         """
-        x = x + self.pe[:x.size(0)]
+        x = x + Variable(self.pe[:x.shape[-2]], requires_grad=False)
         return self.dropout(x)
 
 class LinearEmbedding(nn.Module):
