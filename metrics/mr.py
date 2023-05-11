1112a96326ea40bf22cf32a35db66b31d58d9ac1
lucas-rib-oli <100383154@alumnos.uc3m.es>
2023-05-11 21:58:18 +0200

    metrics: adapt the metrics to a single agent

--- metrics/mr.py
+++ metrics/mr.py
@@ -14,10 +14,8 @@ class MR ( ):
         """_summary_
 
         Args:
-            pred_traj (torch.Tensor): Predicted trajectory [bs, K, pred_len, 2]
+            pred_traj (torch.Tensor): Predicted trajectory [bs, pred_len, 2]
             gt_traj (torch.Tensor): GT trajectory [bs, pred_len, 2]
         """
-        num_traj = pred_traj.shape[1]
-        gt_traj_overdim = gt_traj.unsqueeze(1).repeat(1, num_traj, 1, 1)
-        mr = (torch.norm(pred_traj[:,:, -1] - gt_traj_overdim[:,:, -1], p=2, dim=-1) > self.miss_threshold).sum(-1).float()
+        mr = (torch.norm(pred_traj[:,:, -1] - gt_traj[:,:, -1], p=2, dim=-1) > self.miss_threshold).sum(-1).float()
         return torch.mean(mr)
